a094d7398f305ebe106bf94140decd17ee462cb0
TuliAfroz <afroztulisadia@gmail.com>
2025-06-26 21:36:34 +0600

    change for supabase

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,12 +12,17 @@ dotenv.config();
 //     `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
 // )
 
+// const pool = new Pool({
+//   host: process.env.PGHOST,
+//   port: process.env.PGPORT,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE,
+// });
+
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // required for Supabase
 });
 
 export default pool;
