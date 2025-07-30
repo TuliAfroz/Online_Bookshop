e8eed1ece9e106a6c5aedd7cfb29af92e1eeede4
TuliAfroz <159311185+TuliAfroz@users.noreply.github.com>
2025-07-30 10:56:25 +0600

    Update giftCardRoutes.js

--- backend/routes/giftCardRoutes.js
+++ backend/routes/giftCardRoutes.js
@@ -5,8 +5,6 @@ const router = express.Router();
 
 router.get('/customer/:customer_id', getGiftcardsByCustomer);
 router.post('/add', addGiftcard);
-<<<<<<< HEAD
+
 export default router;
-=======
-export default router;
->>>>>>> 9befe7598735a3022aa97bd60806261323617a01
+
